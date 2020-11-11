2e9b35d98e946ff9160259da807fe153f75cb922
Ville-Veikko Viertola <N4690@student.jamk.fi>
2020-11-11 20:45:21 +0200

    upd

--- resources/views/pages/homework/h9/main.blade.php
+++ resources/views/pages/homework/h9/main.blade.php
@@ -13,38 +13,12 @@
         <div class="content-item">
         <h1>Tehtävä 1 [15p]</h1>
             <div>
-                <p>Käytännössä sama linkki ku
+                <p>Käytännössä sama linkki kun tehtävän 8
                 <br>
                 <a href="{{url('/student')}}" target="_blank"><b>Studetronic</b></a>
                 <br>
 
-            </div>
-               <h1>Tehtävä 2 [5p]</h1>
-               <h3>Pagination</h3>
-            <div>
-                <p>Tuonne taulukon loppuun kun pamautti students->links() nii saatiin näkyviin sivut</p>
-                <div class="center">
-                <div class="zoom"><img src="{{url('images/h8/3.png')}}" alt="Pic" class="center"></div> 
-                </div>
-                <br>
-                 <div class="center">
-                <div class="zoom"><img src="{{url('images/h8/4.png')}}" alt="Pic" class="center"></div> 
-                </div>
-                <br>
-            </div>
-                 <h1>Tehtävä 3 [5p]</h1>
-               <h3>Kurssit</h3>
-            <div>
-                <p>Tuo materiaalin haku oli melkein valmis, piti vain vaihtaa where->course.id = $id ja pientä muutosta foreach looppiin</p>
-                <div class="center">
-                <div class="zoom"><img src="{{url('images/h8/2.png')}}" alt="Pic" class="center"></div> 
-                </div>
-                <br>
-                 <div class="center">
-                <div class="zoom"><img src="{{url('images/h8/1.png')}}" alt="Pic" class="center"></div> 
-                </div>
-                <br>
-            </div>
+   
         </div>
     </div>
 </div>
